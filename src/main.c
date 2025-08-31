8edc36218532dd7580e7650b0646a9bb7d3a3441
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-31 20:31:03 +0400

    Добавил перевод строки!

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@ int main() {
 	printf("Hello, world! 😸\n");
 	int x = 42;
 	int* ptr = &x;
-	printf("Значение перемменой x через указатель: %d", *ptr);
+	printf("Значение перемменой x через указатель: %d\n", *ptr);
 	*ptr = 43;
-	printf("Теперь значение x через указатель: %d", *ptr);
+	printf("Теперь значение x через указатель: %d\n", *ptr);
 	return 0;
 }
