89d070b70936de08bddd9c4db86e45194d841601
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-31 19:29:53 +0400

    Добавил указатели

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello, world! 😸");
+	printf("Hello, world! 😸\n");
+	int x = 42;
+	int* ptr = &x;
+	printf("Значение перемменой x через указатель: %d", *ptr);
+	*ptr = 43;
+	printf("Теперь значение x через указатель: %d", *ptr);
 	return 0;
 }
